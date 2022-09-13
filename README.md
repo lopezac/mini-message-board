7cfc6b9cff97bac925d7efe983bf7922c52fd496
Axel Carlos Lopez <lopezaxel@protonmail.com>
2022-09-13 12:05:01 -0300

    Finish basic page

---
+++ README.md
@@ -0,0 +1,3 @@
+# mini-message-board
+
+Basic message board app, written in Express, NodeJS using MongoDB, Mongoose and Pug.
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
-var express = require('express');
+const { format } = require("date-fns");
+var express = require("express");
+
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hello world!",
+    user: "Juan",
+    added: format(new Date(), "PPpp"),
+  },
+  {
+    text: "Aun que no te pueda ver",
+    user: "Alex Ubago",
+    added: format(new Date(), "PPpp"),
+  },
+  {
+    text: "Tu eres mi religion",
+    user: "Mana",
+    added: format(new Date(), "PPpp"),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Message Board", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+router.post("/new", function (req, res, next) {
+  const { msgAuthor, msgText } = req.body;
+  messages.push({
+    text: msgText,
+    user: msgAuthor,
+    added: format(new Date(), "PPpp"),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
